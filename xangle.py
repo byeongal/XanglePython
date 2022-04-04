750880327ad73a767c3e0469c260b10092631978
YoungJae Kim <byeongal@comcom.ai>
2022-04-04 19:16:10 +0900

    update get_disclosure_detail

--- xangle.py
+++ xangle.py
@@ -110,15 +110,81 @@ class Xangle(object):
                 params={
                     "search_type": search_type,
                     "symbol": symbol,
-                    "token_address": token_address,
-                },
+                    "token_address": token_address
+                }
             )
             return response.json()
         except requests.exceptions.ConnectionError as e:
             return {"error": f"{e}"}
 
     # https://pro-api.xangle.io/#operation/disclosure-detail
-    def get_disclosure_detail(self, disclosure_id: str, lang: str = "en") -> Dict:
+    def get_disclosure_detail(self, disclosure_id: str, lang: Literal["en", "ko"] = "en") -> Dict:
+        """
+        Contents of the disclosure_id
+        Args:
+            disclosure_id (str): id of the disclosure
+            lang (Literal["en", "ko"], optional): language specification. Defaults to "en".
+        Returns:
+            Dict: application/json Object about information of disclosure
+        Example:
+            >>> get_disclosure_detail("624ab34956d727e7126da0a9")
+            {
+                "data": {
+                    "project_id": "5cde464e602c16381e881010",
+                    "project_symbol": "ETH",
+                    "project_logo": "https://s3.ap-northeast-2.amazonaws.com/upload.xangle.io/images/project/5cde464e602c16381e881010/32.png",
+                    "title": "Public Vulnerability Disclosures: Protecting the Ethereum consensus-layer",
+                    "publish_status": "public",
+                    "publish_timestamp": "2022-03-10T06:11:14.788000+00:00",
+                    "publish_timestamp_utc": "2022-03-10T06:11:14.788000",
+                    "last_modified_timestamp": None,
+                    "valid": True,
+                    "disclosure_type": "standard",
+                    "resolved": False,
+                    "validation_method": "not_applicable",
+                    "meta": {
+                    "official_company_name": "Ethereum",
+                    "jurisdiction": "switzerland",
+                    "address": "Zug, Switzerland (Ethereum Foundation)",
+                    "website_url": "https://ethereum.org"
+                    },
+                    "data": {
+                    "title": "Public Vulnerability Disclosures: Protecting the Ethereum consensus-layer",
+                    "subjects": [
+                        {
+                        "subject": "Public Vulnerability Disclosures: Protecting the Ethereum consensus-layer",
+                        "date_start": "2022-03-09T00:00:00",
+                        "date_end": None,
+                        "details": "<p>On March 9, 2022, the Ethereum Foundation announced that it has disclosed the first set of vulnerabilities from the Ethereum Foundation\u2019s Bug Bounty Programs. These vulnerabilities were previously discovered and reported directly to the Ethereum Foundation or client teams via the Bug Bounty Programs for both the Execution Layer and Consensus Layer.</p><p>Through its Bug Bounty Programs, which allow the Ethereum Foundation to coordinate and cross-check vulnerabilities across clients, the foundation currently accepts vulnerability reports for Nimbus, Teku, Lighthouse, Prysm, Lodestar, Go Ethereum, Nethermind, Erigon and Besu.</p><p><i>For more information, and to learn more about disclosure policies, timelines, and cataloging, head over to the new&nbsp;</i><a href=\"https://github.com/ethereum/public-disclosures/\"><i>disclosures repository</i></a><i>.</i></p>"
+                        }
+                    ],
+                    "associated_links": [
+                        {
+                        "title": "Ethereum - Announcement",
+                        "url": "https://blog.ethereum.org/2022/03/09/secured-no-2/"
+                        }
+                    ],
+                    "files": [
+                        {
+                        "title": "Ethereum - Announcement.pdf",
+                        "url": "https://s3.ap-northeast-2.amazonaws.com/upload.xangle.io/files/project/5cde464e602c16381e881010/disclosure/Ethereum_-_Announcement-975a1959-ba9d-4a0f-8958-35d732758518.pdf"
+                        }
+                    ]
+                    },
+                    "details": "\n            \n                        <h3>1. Subject</h3>\n\n                        <p>Public Vulnerability Disclosures: Protecting the Ethereum consensus-layer</p></br>\n\n                    \n                        <h3>2. Applicable Date(s)</h3>\n\n                        <p>2022-03-09 ~ </p></br>\n\n                    \n                        <h3>3. Details</h3>\n\n                        <p><p>On March 9, 2022, the Ethereum Foundation announced that it has disclosed the first set of vulnerabilities from the Ethereum Foundation\u2019s Bug Bounty Programs. These vulnerabilities were previously discovered and reported directly to the Ethereum Foundation or client teams via the Bug Bounty Programs for both the Execution Layer and Consensus Layer.</p><p>Through its Bug Bounty Programs, which allow the Ethereum Foundation to coordinate and cross-check vulnerabilities across clients, the foundation currently accepts vulnerability reports for Nimbus, Teku, Lighthouse, Prysm, Lodestar, Go Ethereum, Nethermind, Erigon and Besu.</p><p><i>For more information, and to learn more about disclosure policies, timelines, and cataloging, head over to the new&nbsp;</i><a href=\"https://github.com/ethereum/public-disclosures/\"><i>disclosures repository</i></a><i>.</i></p></p></br>\n\n                    \n        ",
+                    "translator": None,
+                    "pdf_file_url": None
+                },
+                "status": {
+                    "timestamp": "2022-04-04T10:03:32.950164",
+                    "error_code": 0,
+                    "error_message": None,
+                    "credit": 1,
+                    "current_credit": 1999990
+                }
+            }
+            
+        """
         try:
             response = requests.get(
                 "https://pro-api.xangle.io/v1/disclosure/detail",
